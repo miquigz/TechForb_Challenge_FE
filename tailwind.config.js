d6a73f552d819e30053ed41b4eea9dfacb6d537d
Miqueas Gimenez <miquigz02@gmail.com>
2023-06-09 21:26:07 -0300

    feat: loader component, home svgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        move: "move 1.5s ease-in-out infinite alternate",
+        movealt: "movealt 1.5s ease-in-out infinite alternate"
+      },
+      keyframes: {
+        move: {
+          to: {
+            transform: ' translateX(-100%)',
+          },
+        },
+        movealt: {
+          to: {
+            transform: ' translateX(100%)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
